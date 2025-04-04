f245774e3e73f6bc6e81cc5b7679ff5e48b22897
Abukawa <abukawa@area.jp>
2025-04-04 11:46:35 +0900

    Next.js　チャプター15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
- 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth'; 
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
   id: z.string(),
@@ -122,6 +124,24 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
   }
 
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
 
   /* export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
